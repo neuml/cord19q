93f374fe97ddd8c254be04c98725f499d2a92f82
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2020-06-07 20:40:01 -0400

    Changed fastText minCount from 5 to 4

--- src/python/cord19q/vectors.py
+++ src/python/cord19q/vectors.py
@@ -173,4 +173,4 @@ class Vectors(object):
 
 if __name__ == "__main__":
     # Create vector model
-    Vectors.run(sys.argv[1] if len(sys.argv) > 1 else None, 300, 5)
+    Vectors.run(sys.argv[1] if len(sys.argv) > 1 else None, 300, 4)
