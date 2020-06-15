b162a58f26b593ad254431a4520497a51e5e3d87
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2020-06-15 14:03:19 -0400

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     DESCRIPTION = f.read()
 
 setup(name="cord19q",
-      version="2.0.0",
+      version="2.1.0",
       author="NeuML",
       description="CORD-19 Analysis",
       long_description=DESCRIPTION,
